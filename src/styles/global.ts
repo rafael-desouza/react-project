622444cafabbd93157dea641951751d740cf4cd1
Rafael Ferreira de Souza <rafael.souza@sisplansistemas.com.br>
2022-01-16 11:03:39 -0300

    feat: imported Poppins font and seted responsives sizes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,7 +18,20 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
     }
+
+    html{
+        @media (max-width: 1080px){
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px){
+            font-size: 60%;
+        }
+    }
+
+
 `
 
 export default GlobalStyle
