21adb9cfdd5f7887ed646ebf0bce47b68e03d67f
Rafael Ferreira de Souza <rafael.souza@sisplansistemas.com.br>
2022-01-16 10:52:44 -0300

    feat: created the initial color theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,24 @@
 import { createGlobalStyle } from 'styled-components'
 
-export default createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
+    :root{
+    --background: #FFFFFF;
+    --surface: #222222
+    --primary: #43BA88;
+    --secondary: #2B2B2B;
+    --onBackground: #222222
+    --onSurface: #FFFFFF
+    --onPrimary:#FFFFFF
+    --onSecondary:#FFFFFF 
+
+    }   
+    
+    
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
     }
 `
+
+export default GlobalStyle
